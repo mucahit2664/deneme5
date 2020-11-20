ca965d48f32f3b914cf01bba97aadf0078cb33e8
Abdullah Mucahit <mucahit.sivasligil123@gmail.com>
2020-11-20 20:00:56 +0300

    aliden mastera

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,6 +1,8 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Birinci komut");
+        System.out.println("Ikinci komut");
+        System.out.println("Ucuncu Commit");
 
     }
 }
